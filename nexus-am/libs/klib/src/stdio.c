625e74657c34b7f382225aa055a8fe8453d49e54
tracer-ics2018 <tracer@njuics.org>
2019-01-09 14:04:41 +0800

    >  gdb U201514747 yanxin Linux ubuntu 4.2.0-42-generic #49~14.04.1-Ubuntu SMP Wed Jun 29 20:22:11 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux  14:04:41 up 15 min,  2 users,  load average: 0.09, 0.14, 0.16 bb74334b444392ca2d4f31404a65533ab7a2908c

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -1,22 +1,293 @@
-#include "klib.h"
 #include <stdarg.h>
+
+#include "klib.h"
+
+
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-int printf(const char *fmt, ...) {
-  return 0;
+
+
+// typedef void (*outer_t)(struct OutputEngine* eng);
+
+
+
+typedef struct OutputEngine {
+
+    char ch;    // mutable
+
+    int index;    // mutable
+
+    void (*fn)(struct OutputEngine*);
+
+    char* buf;
+
+    int maxlen;
+
+} OutputEngine;
+
+
+
+static void buf_w(OutputEngine* eng) {
+
+    char ch = eng->ch;
+
+    int index = eng->index;
+
+    int maxlen = eng->maxlen;
+
+    if(index < maxlen - 1) {
+
+        eng->buf[index] = ch;
+
+    } else if(index == maxlen - 1){
+
+        eng->buf[index] = '\0';
+
+    }
+
 }
 
-int vsprintf(char *out, const char *fmt, va_list ap) {
-  return 0;
+
+
+static void io_w(OutputEngine* eng) {
+
+    char ch = eng->ch;
+
+    _putc(ch);    
+
 }
 
-int sprintf(char *out, const char *fmt, ...) {
-  return 0;
+
+
+static void exec(OutputEngine* eng, char ch, int index) {
+
+    eng->ch = ch;
+
+    eng->index = index;
+
+    eng->fn(eng);
+
 }
 
-int snprintf(char *out, size_t n, const char *fmt, ...) {
-  return 0;
+
+
+int atoi_internal(char* buf, int x_) {
+
+    int prefix = 0;
+
+    unsigned x = x_;
+
+    if(x < 0) {
+
+        buf[0] = '-';
+
+        x = -x_;
+
+        prefix = 1;
+
+    } else if(x == 0) {
+
+        buf[0] = '0';
+
+        return 1;
+
+    }
+
+    int idx = prefix; 
+
+    while(x > 0){
+
+       buf[idx] = x % 10 + '0';
+
+       x /= 10;
+
+       ++idx;
+
+    }
+
+    int iter1 = prefix; 
+
+    int iter2 = idx-1;
+
+    while(iter1 < iter2){
+
+        char tmp = buf[iter1];
+
+        buf[iter1] = buf[iter2];
+
+        buf[iter2] = tmp;
+
+        ++iter1;
+
+        --iter2;
+
+    }
+
+    return idx;
+
 }
+
+
+
+int handler(OutputEngine* eng, const char* fmt, va_list va) {
+
+    int dest_idx = 0;
+
+    while(*fmt) {
+
+        if(*fmt != '%') {
+
+            exec(eng, *fmt, dest_idx++);
+
+            ++fmt;
+
+            continue;
+
+        }
+
+        assert(*fmt == '%');
+
+        ++fmt;
+
+        switch(*fmt) { 
+
+            case 'd': {
+
+                int x = va_arg(va, int);
+
+                char buf[30];
+
+                const int delta = atoi_internal(buf, x);
+
+                for(int i = 0; i < delta; ++i){
+
+                    exec(eng, buf[i], dest_idx++);
+
+                }
+
+                ++fmt;
+
+                continue;
+
+            }
+
+            case 's': {
+
+                const char* str = va_arg(va, char*);
+
+                while(*str) {
+
+                    exec(eng, *str++, dest_idx++);
+
+                }
+
+                ++fmt;
+
+                continue;
+
+            }
+
+            default: goto FUCK;
+
+        }
+
+    }
+
+    exec(eng, 0, dest_idx++);
+
+FUCK:
+
+    return dest_idx - 1;
+
+}
+
+
+
+
+
+int printf(const char* fmt, ...) {
+
+    OutputEngine eng;
+
+    eng.fn = io_w;
+
+    va_list va; 
+
+    va_start(va, fmt);
+
+    int ref = handler(&eng, fmt, va);
+
+    va_end(va);
+
+    return ref;
+
+}
+
+
+
+int vsprintf(char* out, const char* fmt, va_list va) {
+
+    OutputEngine eng;
+
+    eng.buf = out;
+
+    eng.maxlen = 1 << 30;
+
+    eng.fn = buf_w;
+
+    return handler(&eng, fmt, va);
+
+}
+
+
+
+int sprintf(char* out, const char* fmt, ...) {
+
+    OutputEngine eng;
+
+    eng.fn = buf_w;
+
+    eng.buf = out;
+
+    eng.maxlen = 1 << 30;
+
+    va_list va; 
+
+    va_start(va, fmt);
+
+    int ref = handler(&eng, fmt, va);
+
+    va_end(va);
+
+    return ref;
+
+}
+
+
+
+int snprintf(char* out, size_t n, const char* fmt, ...) {
+
+    OutputEngine eng;
+
+    eng.fn = buf_w;
+
+    eng.buf = out;
+
+    eng.maxlen = n;
+
+    va_list va; 
+
+    va_start(va, fmt);
+
+    int ref = handler(&eng, fmt, va);
+
+    va_end(va);
+
+    return ref;
+
+}
+
+
 
 #endif
--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -1,41 +1,151 @@
 #include "klib.h"
 
+
+
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-size_t strlen(const char *s) {
-  return 0;
+
+
+size_t strlen(const char* s) {
+
+    int n = 0;
+
+    while((s[n])) ++n;
+
+    return n;
+
 }
 
-char *strcpy(char* dst,const char* src) {
-  return NULL;
+
+
+char* strcpy(char* dst, const char* src) {
+
+    char* iter = dst;
+
+    while((*iter++ = *src++)){}
+
+    return dst;
+
 }
+
+
 
 char* strncpy(char* dst, const char* src, size_t n) {
-  return NULL;
+
+    char* iter = dst;
+
+    while((n-- && (*iter++ = *src++))){}
+
+    return dst;
+
 }
+
+
 
 char* strcat(char* dst, const char* src) {
-  return NULL;
+
+    char* iter = dst;
+
+    while((*iter))++iter;
+
+    while((*iter++ = *src++)){}
+
+    return dst;
+
 }
+
+
 
 int strcmp(const char* s1, const char* s2) {
-  return 0;
+
+    return strncmp(s1, s2, -1);    
+
 }
+
+
 
 int strncmp(const char* s1, const char* s2, size_t n) {
-  return 0;
+
+    for(int i = 0 ; i < n; ++i){
+
+        if(s1[i] == s2[i] && s1[i]) {
+
+            continue;
+
+        }
+
+        else {
+
+            return s1[i] - s2[i];
+
+        }
+
+    }
+
+    return 0;
+
 }
 
-void* memset(void* v,int c,size_t n) {
-  return NULL;
+
+
+void* memset(void* v, int c, size_t n) {
+
+    for(int i = 0; i < n; ++i){
+
+        ((char* )v)[i] = (char)c;
+
+    }
+
+    return v;
+
 }
+
+
 
 void* memcpy(void* out, const void* in, size_t n) {
-  return NULL;
+
+    for(int i = 0; i < n; ++i){
+
+        ((char*)out)[i] = ((char*)in)[i]; 
+
+    }
+
+    return out;
+
 }
 
-int memcmp(const void* s1, const void* s2, size_t n){
-  return 0;
+
+
+int memcmp(const void* s1, const void* s2, size_t n) {
+
+    const char* left_ss = (const char*)s1;
+
+    const char* right_ss = (const char*)s2;
+
+
+
+    for(int i = 0; i < n; ++i) {
+
+        char left = left_ss[i];
+
+        char right = right_ss[i];
+
+        if(left == right) {
+
+            continue;
+
+        } else {
+
+            return left - right;
+
+        }
+
+    }
+
+    return 0;
+
 }
+
+
 
 #endif
