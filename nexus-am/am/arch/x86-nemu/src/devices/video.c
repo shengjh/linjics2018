b37e2016404ed4490ec35552261fd6a0b17c93b6
tracer-ics2018 <tracer@njuics.org>
2019-01-10 01:23:14 +0800

    >  run U201514747 linjian Linux ubuntu 4.10.0-28-generic #32~16.04.2-Ubuntu SMP Thu Jul 20 10:19:48 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  01:23:14 up  3:03,  1 user,  load average: 0.93, 0.45, 0.31 baa0cbda796d9af885dc4def9479c19782cf12

--- nexus-am/am/arch/x86-nemu/src/devices/video.c
+++ nexus-am/am/arch/x86-nemu/src/devices/video.c
@@ -1,63 +1,89 @@
 #include <am.h>
-#include <x86.h>
+
 #include <amdev.h>
+
 #include <klib.h>
 
+#include <x86.h>
 
 
-int H , W;
+
+#define SCREEN_PORT 0x100
 
 
-static inline int min(int x, int y) {
-  return (x < y) ? x : y;
-}
+
+static uint32_t *const fb __attribute__((used)) = (uint32_t *)0x40000;
 
 
-static uint32_t* const fb __attribute__((used)) = (uint32_t *)0x40000;
 
 size_t video_read(uintptr_t reg, void *buf, size_t size) {
-  switch (reg) {
-    case _DEVREG_VIDEO_INFO: {
-      _VideoInfoReg *info = (_VideoInfoReg *)buf;
-      uint32_t screen;
-      screen = inl(0x100);
-      W = info->width = screen >> 16;
-      H = info->height = screen << 16 >> 16;
-      return sizeof(_VideoInfoReg);
+
+    switch(reg) {
+
+        case _DEVREG_VIDEO_INFO: {
+
+            _VideoInfoReg *info = (_VideoInfoReg *)buf;
+
+            uint32_t data = inl(SCREEN_PORT);
+
+            info->width = data >> 16;
+
+            info->height = data & 0xFFFF;
+
+            return sizeof(_VideoInfoReg);
+
+        }
+
     }
-  }
-  return 0;
+
+    return 0;
+
 }
+
+
 
 size_t video_write(uintptr_t reg, void *buf, size_t size) {
-  switch (reg) {
-    case _DEVREG_VIDEO_FBCTL: {
-      _FBCtlReg *ctl = (_FBCtlReg *)buf;
-	//int i;   
-	//int size = screen_width() * screen_height();
-       // for (i = 0; i < size; i ++) 
-       //   fb[i] = i;
-       //int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
-       //uint32_t *pixels = ctl->pixels;
-       //int cp_bytes = sizeof(uint32_t) * min(w, W - x);
-      //  for (int j = 0; j < h && y + j < H; j ++) {
-      //   memcpy(&fb[(y + j) * W + x], pixels, cp_bytes);
-      //   pixels += w;
-      // }
-      int i;
-			for(i = 0;i < ctl->h;i ++)
-			{
-			  memcpy(fb+(ctl->y+i)*screen_width()+ctl->x,ctl->pixels+i*ctl->w,ctl->w*4);
-			}
-      if (ctl->sync) {
-        // do nothing, hardware syncs.
 
-      }
-      return sizeof(_FBCtlReg);
+    switch(reg) {
+
+        case _DEVREG_VIDEO_FBCTL: {
+
+            _FBCtlReg *ctl = (_FBCtlReg *)buf;
+
+            
+
+            // int size = screen_width() * screen_height();
+
+            int W = screen_width();
+
+            uint32_t* base = fb + ctl->y * W + ctl->x;
+
+            for(int dy = 0; dy < ctl->h; ++dy){
+
+                for(int dx = 0; dx < ctl->w; ++dx){
+
+                    base[dy * W + dx] = ctl->pixels[dy * ctl->w + dx]; 
+
+                }
+
+            }
+
+            if(ctl->sync) {
+
+                // do nothing, hardware syncs.
+
+            }
+
+            return sizeof(_FBCtlReg);
+
+        }
+
     }
-  }
-  return 0;
+
+    return 0;
+
 }
 
-void vga_init() {
-}
+
+
+void vga_init() {}
